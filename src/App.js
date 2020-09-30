d0317dc62bf2d048f3a3285f7b0d0e6ffb50a463
Hans Marcon <yourcapslock@gmail.com>
2020-09-30 23:39:15 +0800

    Initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './bootstrap.min.css';
+//import './jquery-3.3.1.min.js';
+//import './bootstrap.min.js';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="whole">
+        
+        <div id="intro_screen" class="card">
+            <div class="card-body" id="intro_text">
+                <h1 id="logo_name">Canvas</h1>
+                <p>Welcome to the Canvas app! In here, you can browse all your classes, view new messages, check for announcements on your classes, among other things. But first, a little introduction on the app!</p>
+                <p><button class="btn btn-lg btn-dark">Let's Get Started!</button></p>
+            </div>
+        </div>
+        
+        <script type="text/javascript" src="jquery-3.3.1.min.js"></script>
+        <script type="text/javascript" src="bootstrap.min.js"></script>
     </div>
+    
   );
 }
 
